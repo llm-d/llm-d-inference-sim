4de53b64eb1b49b1d20f69fcb7da3f5fdef8f606
Maya Barnea <mayab@il.ibm.com>
2025-07-24 10:19:05 +0300

    - Use tokenize function which divide text by space and additional characters in request processing too (not only in tools related part) - Validate max_token and max_completion_token as request arrived - Protect generating any random value with mutex - Fix test for the changes above + add test for random texts creation
    Signed-off-by: Maya Barnea <mayab@il.ibm.com>

--- pkg/llm-d-inference-sim/utils_test.go
+++ pkg/llm-d-inference-sim/utils_test.go
@@ -17,7 +17,7 @@ limitations under the License.
 package llmdinferencesim
 
 import (
-	"strings"
+	"fmt"
 	"time"
 
 	. "github.com/onsi/ginkgo/v2"
@@ -32,20 +32,43 @@ var _ = Describe("Utils", Ordered, func() {
 	Context("GetRandomResponseText", func() {
 		It("should return complete text", func() {
 			text, finishReason := getRandomResponseText(nil)
-			Expect(text).Should(Equal(getFullTextFromPartialString(text)))
+			Expect(int64(len(tokenize(text)))).Should(BeNumerically("<=", ResponseLenMax))
+			Expect(finishReason).Should(Equal(stopFinishReason))
+		})
+		It("should return short text", func() {
+			maxCompletionTokens := int64(2)
+			text, finishReason := getRandomResponseText(&maxCompletionTokens)
+			Expect(int64(len(tokenize(text)))).Should(Equal(maxCompletionTokens))
+			Expect([]string{stopFinishReason, lengthFinishReason}).Should(ContainElement(finishReason))
+		})
+		It("should return long text", func() {
+			// return required number of tokens although it is higher than ResponseLenMax
+			maxCompletionTokens := int64(ResponseLenMax * 5)
+			text, finishReason := getRandomResponseText(&maxCompletionTokens)
+			Expect(int64(len(tokenize(text)))).Should(Equal(maxCompletionTokens))
+			Expect([]string{stopFinishReason, lengthFinishReason}).Should(ContainElement(finishReason))
+		})
+	})
+
+	Context("GetResponseText", func() {
+		theText := "Give a man a fish and you feed him for a day; teach a man to fish and you feed him for a lifetime"
+
+		It("should return the same text since max tokens is not defined", func() {
+			text, finishReason := getResponseText(nil, theText)
+			Expect(text).Should(Equal(theText))
+			Expect(finishReason).Should(Equal(stopFinishReason))
+		})
+		It("should return the same text since max tokens is higher than the text length", func() {
+			maxCompletionTokens := int64(1000)
+			text, finishReason := getResponseText(&maxCompletionTokens, theText)
+			Expect(text).Should(Equal(theText))
 			Expect(finishReason).Should(Equal(stopFinishReason))
 		})
 		It("should return partial text", func() {
 			maxCompletionTokens := int64(2)
-			text, finishReason := getRandomResponseText(&maxCompletionTokens)
-			Expect(int64(len(strings.Fields(text)))).Should(Equal(maxCompletionTokens))
+			text, finishReason := getResponseText(&maxCompletionTokens, theText)
+			Expect(int64(len(tokenize(text)))).Should(Equal(maxCompletionTokens))
 			Expect(finishReason).Should(Equal(lengthFinishReason))
-		})
-		It("should return complete text", func() {
-			maxCompletionTokens := int64(2000)
-			text, finishReason := getRandomResponseText(&maxCompletionTokens)
-			Expect(text).Should(Equal(getFullTextFromPartialString(text)))
-			Expect(finishReason).Should(Equal(stopFinishReason))
 		})
 	})
 
@@ -91,5 +114,18 @@ var _ = Describe("Utils", Ordered, func() {
 			Expect(actualCompletionTokens).Should(Equal(int64(0)))
 			Expect(totalTokens).Should(Equal(int64(250)))
 		})
+	})
+
+	Context("GetRandomText", func() {
+		var lenArr []int = []int{5, 20, 50, 150}
+
+		for _, len := range lenArr {
+			name := fmt.Sprintf("should return text with %d tokens", len)
+			It(name, func() {
+				text := getRandomText(len)
+				fmt.Printf("Text with %d tokens: '%s'\n", len, text)
+				Expect(tokenize(text)).Should(HaveLen(len))
+			})
+		}
 	})
 })
